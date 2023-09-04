1c4b9c6cc5a4210ec1bda254c28af307d6580624
euRobertoDias <hombrous@gmail.com>
2023-09-04 10:20:18 -0300

    Desafio Iniciando com javascript

---
+++ main.js
@@ -0,0 +1,61 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+// alert("Hello World!")
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+// let number1 = 1
+// let number2 = 2
+// alert(`${number1} + ${number2} = ${number1 + number2}`)
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+// 💡 Para saber o tipo de dado você pode usar o operador `typeof`
+// let isNumber = "4"
+// typeof isNumber === "number" ? alert("É um número") : alert("Não é um número")
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+// let isString = "Não é uma string"
+// if (typeof isString === "string") {
+//     alert("É uma string")
+// } else {
+//     alert("Não é uma string")
+// } 
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+// let isBoolean = false
+// switch (typeof isBoolean) {
+//     case "boolean":
+//         alert("É um boleano")
+//         break;
+//     case "string":
+//         alert("Não é um boleano")
+//         break;
+//     default:
+//         alert("Não é um boleano")
+// }
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+// let n1 = 10
+// let n2 = 3
+// alert(n1 + " - " + n2 + " = " + (n1 - n2))
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+// let n1 = 15
+// let n2 = 2
+// alert(`${n1} + ${n2} = ${n1 * n2}`)
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+// let n1 = 15
+// let n2 = 2
+// alert(`${n1} + ${n2} = ${n1 / n2}`)
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+// let number = 4
+// if (number % 2 == 0) {
+//     alert("É um número par")
+
+// } else {
+//     alert("Não é um número par")
+// }
+
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+// let n = 10
+// n % 2 == 0 ? alert("Nãa é um número impar") : alert("É um número impar")
